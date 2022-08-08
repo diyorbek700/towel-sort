db6e7d6f6404826a6f2b7df117b1cc650059c512
diyorbek <104645006+diyorbek700@users.noreply.github.com>
2022-08-08 08:01:58 +0500

    Update index.js

--- src/index.js
+++ src/index.js
@@ -2,5 +2,25 @@
 // You should implement your task here.
 
 module.exports = function towelSort (matrix) {
-  return [];
+  
+     console.log('\nSnake bypass:');
+  
+    for (let i = 0; i < matrix.length; i++) {
+      for (let j = 0; j < matrix[i].length; j++) {
+        let columnIdx = i % 2 === 0 
+          ? j
+          : (matrix[i].length - j - 1);
+
+
+          
+        console.log( matrix[i][columnIdx] );
+      }
+    }
 }
+ let smallMatrix = [
+    [1, 2, 3],
+    [4, 5, 6],
+    [7, 8, 9],
+  ];
+  showMatrix(smallMatrix);
+  snakeBypass(smallMatrix);
